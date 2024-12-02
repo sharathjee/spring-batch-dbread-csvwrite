03459b3cb8ad2629af5601a9c83215244813045b
Kumar <sharath.g.kumar@capgemini.com>
2024-12-02 17:19:07 +0530

    first commit

---
+++ src/main/java/com/discover/SpringBatchDbreadCsvwriteApplication.java
@@ -0,0 +1,13 @@
+package com.discover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDbreadCsvwriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchDbreadCsvwriteApplication.class, args);
+	}
+
+}
